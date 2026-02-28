3dfa274c1cf9ae4b2e951a91ccc9a91efe7bd9ed
CoreDumpNotCrash <nazarkolepkov008@gmail.com>
2026-02-28 15:30:36 +0200

    ft: added music on the background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import playSound from "./tools/playSound";
 
 import artfulImage from "./assets/image.png";
 import hitSound from "./assets/hit.mp3";
 import buySound from "./assets/buy.mp3";
 import discardSound from "./assets/no.mp3";
+import music from "./assets/background_music.mp3";
 
 import "./index.css";
 
@@ -13,10 +14,28 @@ export function App() {
   const [multiplayer, setMupliplayer] = useState(0);
   const [upgradeCost, setUpgradeCost] = useState(50);
 
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio(music));
+
   function artfulOnClick() {
     playSound(hitSound);
     setBeatsUp((prev) => prev + Math.pow(2, multiplayer));
   }
+
+  const toggleMusic = () => {
+    const audio = audioRef.current;
+
+    if (isPlaying) {
+      audio.pause();
+      audio.currentTime = 0;
+    } else {
+      audio.loop = true;
+      audio.volume = 0.5;
+      audio.play().catch(() => {});
+    }
+
+    setIsPlaying(!isPlaying);
+  };
 
   function upgradeOnClick() {
     if (beatsUp < upgradeCost) {
@@ -31,6 +50,9 @@ export function App() {
 
   return (
     <div className="app">
+      <h3>
+        "if you came from discord you are smelly poo fart" - CoreDumpNotCrash
+      </h3>
       <h1>Beat up Artful!!!</h1>
       <p>(hint: click on the artful if that's not obvious)</p>
       <button onClick={artfulOnClick} className="button">
@@ -40,7 +62,7 @@ export function App() {
       <button onClick={upgradeOnClick} className="button">
         Upgrade({upgradeCost} clicks)
       </button>
-      <h1>if you came from discord you are smelly poo fart</h1>
+      <button onClick={toggleMusic}>Toggle Music</button>
     </div>
   );
 }
