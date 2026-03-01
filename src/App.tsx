19fcd41bc321dab708a4b6fcc7b1d9512dc3939c
CoreDumpNotCrash <nazarkolepkov008@gmail.com>
2026-03-01 13:07:19 +0200

    ft: added its own div and room for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,8 @@ export function App() {
           setMultiplayer={setMultiplayer}
           multiplayer={multiplayer}
         />
+      </div>
+      <div>
         <ToggleMusicComponent
           audioRef={audioRef}
           isPlaying={isPlaying}
